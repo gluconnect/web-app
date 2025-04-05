673310c244775e185cb50973d7a99a1470bbf240
MinecraftJack64 <48302222+MinecraftJack64@users.noreply.github.com>
2025-04-05 13:34:20 -0700

    small styling changes

--- web/app/inject-navbar.js
+++ web/app/inject-navbar.js
@@ -2,11 +2,10 @@ function go(page){
     window.location.href = "../"+page+"/index.html"; // Redirect to the specified page
 }
 let navbar = document.createElement("footer");
+navbar.id = "nav";
 navbar.innerHTML = `
-<footer id="nav">
         <button id="home" onclick="go('home')">Home</button>
         <button id="glucometers" onclick="go('glucometers')">Connect</button>
         <button id="users" onclick="go('users')">Patients and Caretakers<br><span style="display: none;" class="warning"><span id="warningCount">0</span> Warning(s)!</span></button>
-        <button id="settings" onclick="go('settings')">Settings</button>
-    </footer>`;
+        <button id="settings" onclick="go('settings')">Settings</button>`;
 document.body.appendChild(navbar);
