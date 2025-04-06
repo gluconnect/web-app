6df42bf5c0b68c454907e65c059f41ede1df23c2
MinecraftJack64 <48302222+MinecraftJack64@users.noreply.github.com>
2025-04-05 23:26:47 -0700

    dark and contrast and more themes

--- web/app/glucometers/script.js
+++ web/app/glucometers/script.js
@@ -1,3 +1,21 @@
+function appendGlucometer(glucometer) {
+    // create a new viewer element
+    let viewerElement = document.createElement("li");
+    viewerElement.innerHTML = `
+        <span style="font-weight: bold;">${glucometer.name}</span>
+        <span style="font-weight: bold; color: ${glucometer.status === "Connected" ? "green" : "red"};">${glucometer.status}</span>
+        <div>
+            <button>Settings</button>
+            <button class="remove" onclick="removeGlucometer('${glucometer.id}')">Remove</button>
+        </div>
+    `; // TODO: Implement settings(allow viewer to change threshold, custom threshold, warning notification methods, etc.)
+    viewerElement.style.display = "flex";
+    viewerElement.style.justifyContent = "space-between";
+    viewerElement.style.alignItems = "center";
+    viewerElement.style.flexDirection = "row";
+    viewerElement.style.width = "100%";
+    document.getElementById("glucometerList").appendChild(viewerElement);
+}
 function addGlucometer() {
     // TODO
 }
